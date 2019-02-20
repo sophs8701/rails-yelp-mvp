9f9dc63cbe16a1ddb2d41c67b73d9cc6faeb6455
sophie spratley <sophie.spratley@yahoo.co.uk>
2019-02-20 17:16:58 +0000

    crud done for restaurant and reviews

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  # create
+  # destroy
+
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :content, presence: true
+  # check lecture
+end
